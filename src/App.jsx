f3b2344a95b7351ea711bac5cf3c5ae8dca81175
Palak Gupta <palakgupta2712@gmail.com>
2021-09-17 10:37:58 +0530

    Github corners added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import GithubCorner from "react-github-corner";
 import Routes from "./Routes/Routes";
 
 function App() {
   return (
     <React.Fragment>
+      <GithubCorner
+        href="https://github.com/palakgupta2712/bored-games"
+        bannerColor="#C51935"
+        octoColor="#fff"
+        size={80}
+        direction="right"
+      />
       <Routes />
     </React.Fragment>
   );
