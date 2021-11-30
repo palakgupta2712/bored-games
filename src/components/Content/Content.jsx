f39fbc2bcec1c26b810ee549ff00b81eec3d5ff1
Palak Gupta <palakg@wharfstreetstrategies.com>
2021-11-30 12:21:48 +0530

    image fix

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -6,10 +6,10 @@ function Content() {
   return (
     <React.Fragment>
       <div className={styles.container}>
-        <Card title="Rock Paper Scissors" image="/img/pic1.png" />
-        <Card title="Tic Tac Toe" image="/img/pic2.png" />
-        <Card title="Trivia Quiz" image="/img/pic3.png" />
-        <Card title="Maths Wizard" image="/img/pic4.png" />
+        <Card title="Rock Paper Scissors" image="./img/pic1.png" />
+        <Card title="Tic Tac Toe" image="./img/pic2.png" />
+        <Card title="Trivia Quiz" image="./img/pic3.png" />
+        <Card title="Maths Wizard" image="./img/pic4.png" />
         {/* <Card title="Matching Cards" /> */}
         {/* <Card title="Hangman" /> */}
       </div>
