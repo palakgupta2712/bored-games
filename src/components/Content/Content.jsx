059c5c9ce3d9c13301a854b1054fb9086562c737
Palak Gupta <palakg@wharfstreetstrategies.com>
2021-11-30 12:28:10 +0530

    image fix

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import styles from "./Content.module.css";
 import Card from "../Card/Card";
-import imgUrl from "../../../img/pic1.png";
+import rockpaperscissors from "../../../img/pic1.png";
+import tictactoe from "../../../img/pic2.png";
+import quiz from "../../../img/pic3.png";
+import mathsWizard from "../../../img/pic4.png";
 
 function Content() {
   return (
     <React.Fragment>
       <div className={styles.container}>
-        <Card title="Rock Paper Scissors" image={imgUrl} />
-        <Card title="Tic Tac Toe" image="./img/pic2.png" />
-        <Card title="Trivia Quiz" image="./img/pic3.png" />
-        <Card title="Maths Wizard" image="./img/pic4.png" />
+        <Card title="Rock Paper Scissors" image={rockpaperscissors} />
+        <Card title="Tic Tac Toe" image={tictactoe} />
+        <Card title="Trivia Quiz" image={quiz} />
+        <Card title="Maths Wizard" image={mathsWizard} />
         {/* <Card title="Matching Cards" /> */}
         {/* <Card title="Hangman" /> */}
       </div>
